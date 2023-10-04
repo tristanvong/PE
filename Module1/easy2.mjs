4df79ec0929737b38443abdc04ea95883e8c462e
tristanvong <146710405+tristanvong@users.noreply.github.com>
2023-10-04 17:27:53 +0200

    Update easy2.mjs

--- Module1/easy2.mjs
+++ Module1/easy2.mjs
@@ -1,4 +1,4 @@
-// Module 1 - Variabelen oefening 1
+// Module 1 - Variabelen oefening 2
 // Initials - Initialen
 console.log('_/_/_/_/_/     _/         _/');
 console.log('   _/           _/      _/');
