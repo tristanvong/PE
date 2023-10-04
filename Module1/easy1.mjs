64417b4af6d800731d245971926ae526d57e2087
tristanvong <146710405+tristanvong@users.noreply.github.com>
2023-10-04 17:26:57 +0200

    made exercise 'easy' 1 & 2

---
+++ Module1/easy1.mjs
@@ -0,0 +1,24 @@
+// Module 1 - Variabelen oefening 1
+// Initials - Initialen
+console.log('_/_/_/_/_/     _/         _/');
+console.log('   _/           _/      _/');
+console.log('  _/            _/     _/');
+console.log(' _/              _/  _/');
+console.log('_/         _/     _/');
+console.log('Code geschreven door Tristan Vong');
+console.log('----------------------------------');
+
+// Import userInput function
+import * as readline from 'node:readline/promises';
+import {stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input,output});
+
+// Variables
+let naam = 'Tristan';
+let leeftijd = 18;
+let favorietSpel = 'GTA V';
+
+// Output
+console.log('Hallo '+ naam +', ik hoor dat je '+ leeftijd +' jaar oud bent en dat je lievelingsgame '+ favorietSpel +' is.');
+
+process.exit();
