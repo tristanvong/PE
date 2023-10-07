20e26ff80960fa40b134456e95464bcd230cde79
tristanvong <146710405+tristanvong@users.noreply.github.com>
2023-10-07 18:26:55 +0200

    made easy exercise 2

---
+++ Module3/easy2.mjs
@@ -0,0 +1,32 @@
+// Module 3 - Lussen - easy - oefening 2
+// Initials - Initialen
+// Tafel van vermenigvuldiging
+console.log('_/_/_/_/_/     _/         _/');
+console.log('   _/           _/      _/');
+console.log('  _/            _/     _/');
+console.log(' _/              _/  _/');
+console.log('_/         _/     _/');
+console.log('Code geschreven door Tristan Vong');
+console.log('----------------------------------');
+
+// Import userInput function
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+//Variables
+
+let getal = 8;
+let i = 1;
+
+// Title
+console.log(`De tafel van ${getal}: `);
+
+// While loop
+while(i <= 10){
+    let uitkomst = getal * i;
+    console.log(`${getal} x ${i} = ${uitkomst}`);
+    i++;
+}
+
+process.exit();
