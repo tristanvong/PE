9550b1891a433506c09eb9e3de87a1151a311e05
tristanvong <146710405+tristanvong@users.noreply.github.com>
2023-10-07 17:26:03 +0200

    made easy exercise 1

---
+++ Module3/easy1.mjs
@@ -0,0 +1,24 @@
+// Module 3 - Lussen - easy - oefening 1
+// Initials - Initialen
+// Even of oneven
+console.log('_/_/_/_/_/     _/         _/');
+console.log('   _/           _/      _/');
+console.log('  _/            _/     _/');
+console.log(' _/              _/  _/');
+console.log('_/         _/     _/');
+console.log('Code geschreven door Tristan Vong');
+console.log('----------------------------------');
+
+// Import userInput function
+import * as readline from 'node:readline/promises';
+import{stdin as input, stdout as output} from 'node:process';
+const userInput = readline.createInterface({input, output});
+
+// Print every number from 1 through 20
+let som = 0;
+while(som <= 19){
+    som += 1
+    console.log(som)
+}
+
+process.exit();
