af8072062713657a968da65942ca29676384ea40
tristanvong <146710405+tristanvong@users.noreply.github.com>
2023-10-14 12:00:36 +0200

    made easy exercise 1

---
+++ Module4/easy1.mjs
@@ -0,0 +1,22 @@
+// Module 4 - Zo zijn we logisch - easy - oefening 1
+// Initials - Initialen
+// Oppervlakte
+console.log('_/_/_/_/_/     _/         _/');
+console.log('   _/           _/      _/');
+console.log('  _/            _/     _/');
+console.log(' _/              _/  _/');
+console.log('_/         _/     _/');
+console.log('Code geschreven door Tristan Vong');
+console.log('----------------------------------');
+console.log('Oppervlakte van een rechthoek meten.')
+
+// Variables
+let zijde1 = 8;
+let zijde2 = 2;
+let result;
+
+// Formula
+result = zijde1*zijde2
+
+// Output
+console.log(`De oppervlakte van de rechthoek is: ${result}`)
